8273f37768e7b56d141052d8620d249210c858b4
CaferPoyrazoglu <cafererhan@gmail.com>
2022-07-29 14:06:38 +0300

    MainNav structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center">Hello World!</h1>
+  <MainNav></MainNav>
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
 export default {
   name: "App",
+  components: { MainNav: MainNav },
 };
 </script>
